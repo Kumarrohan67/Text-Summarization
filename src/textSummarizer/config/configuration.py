6185035cc1507f19497f56ca36ee7fa012cb35a3
Rohan Kumar <rohansinghchouhan2@gmail.com>
2024-08-10 17:59:11 +0530

    Trainig via GPU is setuped

--- src/textSummarizer/config/configuration.py
+++ src/textSummarizer/config/configuration.py
@@ -1,6 +1,6 @@
 from textSummarizer.constants import *
 from textSummarizer.utils.common import read_yaml, create_directory
-from textSummarizer.entity import (DataIngestionConfig,ModelTrainerConfig,DataValidationConfig,DataTransformationConfig)
+from textSummarizer.entity import (DataIngestionConfig, ModelEvaluationConfig , ModelTrainerConfig,DataValidationConfig,DataTransformationConfig)
 
 
 
@@ -75,4 +75,20 @@ class configurationManager:
               gradient_accumulation_step=params.gradient_accumulation_step
           )
           return model_trainer_config
+    
+
+    def get_model_evaluation_config(self) -> ModelEvaluationConfig:
+          config= self.config.model_evaluation
+
+          create_directory([config.root_dir])
+
+          model_evaluation_config = ModelEvaluationConfig(
+                root_dir = config.root_dir,
+                data_path = config.data_path,
+                model_path = config.model_path,
+                tokenizer_path = config.tokenizer_path,
+                metric_file_name = config.metric_file_name
+          )
+
+          return model_evaluation_config
 
--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -39,3 +39,11 @@ class ModelTrainerConfig:
     eval_steps: int
     save_steps: float
     gradient_accumulation_step: int
+
+@dataclass(frozen=True)
+class ModelEvaluationConfig:
+    root_dir: Path
+    data_path: Path
+    model_path: Path
+    tokenizer_path: Path
+    metric_file_name: Path
